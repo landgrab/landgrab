789da819a211e63af87a1c87f2124af343efbec9
Ollie Bennett <olliebennett@gmail.com>
2024-11-11 22:10:22 +0000

    Fix register redirect

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -12,7 +12,7 @@ class CheckoutController < ApplicationController
   # See docs/CHECKOUT.md
   def checkout
     unless user_signed_in?
-      redirect_to new_registration_path,
+      redirect_to new_registration_path(:user),
                   flash: { notice: 'Please register so we can link the subscription to an account' }
     end
 
