e032939013849df424bfe2b5bf920f040447a318
Ollie Bennett <olliebennett@gmail.com>
2025-05-18 20:37:01 +0100

    RuboCop: Style/RedundantParentheses fix

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -4,7 +4,7 @@ class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if email_valid?(value)
 
-    record.errors.add(attribute, (options[:message] || 'must be a valid email address'))
+    record.errors.add(attribute, options[:message] || 'must be a valid email address')
   end
 
   def email_valid?(value)
