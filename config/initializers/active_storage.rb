b250b89fb43881045ff5f070a1b3523a829c307f
Ollie Bennett <olliebennett@gmail.com>
2025-05-19 21:07:04 +0100

    RuboCop

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -4,9 +4,9 @@ case ENV['ACTIVE_STORAGE_SERVICE_PROVIDER']&.downcase&.to_sym
 when nil
   # not configured; ignore
 when :cloudinary
-    require 'cloudinary'
+  require 'cloudinary'
 
-    Cloudinary.config_from_url(ENV.fetch('CLOUDINARY_URL'))
+  Cloudinary.config_from_url(ENV.fetch('CLOUDINARY_URL'))
 else
-    raise "ActiveStorage service provider '#{ENV['ACTIVE_STORAGE_SERVICE_PROVIDER']}' is not handled"
+  raise "ActiveStorage service provider '#{ENV.fetch('ACTIVE_STORAGE_SERVICE_PROVIDER', nil)}' is not handled"
 end
