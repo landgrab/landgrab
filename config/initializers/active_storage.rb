2fc3bdbc826dd3ac4d644eec49be6dc765c4ab5d
Ollie Bennett <olliebennett@gmail.com>
2025-05-20 05:34:37 +0100

    Allow s3 config without erroring

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -7,6 +7,8 @@ when :cloudinary
   require 'cloudinary'
 
   Cloudinary.config_from_url(ENV.fetch('CLOUDINARY_URL'))
+when :s3
+  # no additional setup needed; see config/storage/production.yml
 else
   raise "ActiveStorage service provider '#{ENV.fetch('ACTIVE_STORAGE_SERVICE_PROVIDER', nil)}' is not handled"
 end
