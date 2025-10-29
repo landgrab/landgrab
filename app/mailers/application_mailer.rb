a5b2efc5eebdee106beb1e5a0a24048db61f5b97
Ollie Bennett <olliebennett@gmail.com>
2025-10-29 00:56:17 +0000

    Remove 'support' suffix from email sender name

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,7 +2,7 @@
 
 class ApplicationMailer < ActionMailer::Base
   # NOTE: If changing this, also update `config.mailer_sender` in `config/initializers/devise.rb`
-  default from: "\"#{ENV.fetch('SITE_TITLE', 'Landgrab')} Support\" <#{LandgrabService.email_from_address}>".freeze
+  default from: "\"#{ENV.fetch('SITE_TITLE', 'Landgrab')}\" <#{LandgrabService.email_from_address}>".freeze
 
   layout 'mailer'
 end
