762cab560a93d69103af4fc4c69e387d8a657e13
Ollie Bennett <olliebennett@gmail.com>
2025-11-03 07:17:27 +0000

    Rename subscription mailer preview for consistency

--- spec/mailers/previews/subscription_mailer_preview.rb
+++ spec/mailers/previews/subscription_mailer_preview.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Preview all emails at http://localhost:3000/rails/mailers/subscription
-class SubscriptionPreview < ActionMailer::Preview
+class SubscriptionMailerPreview < ActionMailer::Preview
   def claim
     subscr = Subscription.new(
       id: 999_999_999,
