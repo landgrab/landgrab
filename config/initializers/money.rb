ee04ae6e0892191499b703d404e56755393f45e6
Ollie Bennett <olliebennett@gmail.com>
2026-01-09 21:50:41 +0000

    Remove now-redundant Money rounding config

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -4,8 +4,6 @@ MoneyRails.configure do |config|
   config.default_currency = :gbp
 
   config.no_cents_if_whole = false
-
-  config.rounding_mode = BigDecimal::ROUND_HALF_UP
 end
 
 Money.locale_backend = :i18n
